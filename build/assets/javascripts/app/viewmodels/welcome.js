caa2962ee0cba843f754c1b6712ece3a62748ed8
Dave Seah <david@davidseah.com>
2015-03-11 01:14:56 -0400

    Update welcome text

--- build/assets/javascripts/app/viewmodels/welcome.js
+++ build/assets/javascripts/app/viewmodels/welcome.js
@@ -3,11 +3,16 @@ define(function() {
         this.displayName = 'About This Project';
         this.description = '<a href="http://davidseah.com/about/make-video-game/">Project 1401</a> is my "make a game" learning project. The goal is to make a game that incorporates graphics, sound, and physics. It doesn\'t really matter that is is a GREAT game; I\'m hopeful that this project will be helpful to others learning how to build a web app/game in Javascript for modern browsers. Along the way I hope I have some fun and put this old dream to rest.';
         this.features = [
-            'Make a clean Durandal/Mimosa shell',
-            'Customize text for project',
-            'Commit installation instructions to Github',
-            'Created documentation wiki on Github',
-            'Added Game Demo Shell'
+            '<b>M0</b> - Game Shell based on Durandal',
+            '<b>M1</b> - Renderer, Pieces, Visuals, SystemLoops ',
+            '<b>M2</b> - Starfield, Physics, Keyboard Controls<br><br>',
+            '<b>M?</b> - Timers, Triggers, Filters, Tests',
+            '<b>M?</b> - Behaviors and State Machines',
+            '<b>M?</b> - Pathing',
+            '<b>M?</b> - Strategic AI',
+            '<b>M?</b> - GUI and Data Overlay',
+            '<b>M?</b> - Stage and Level Management',
+            '<b>M?</b> - Sound'
         ];
     };
 
